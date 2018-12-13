8fcc4a42b772503404e1895933818a5853a69887
Andrew Domanski <andqok@gmail.com>
2018-12-13 05:07:04 +0200

    add images

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express')
 const path = require('path')
 
 var app = express()
+var imgPath = path.join(__dirname, '/img')
+app.use('/img', express.static(imgPath) )
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'))
