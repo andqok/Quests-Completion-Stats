c8100b3cbbb3e37f0c5addc1a057e029ec155888
Andrew Domanski <andqok@gmail.com>
2018-12-15 05:09:51 +0200

    add basic functionality in GameInfo page

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 
 var app = express()
+
 var imgPath = path.join(__dirname, '/img')
 app.use('/img', express.static(imgPath) )
 
@@ -13,6 +15,18 @@ app.get('/', function (req, res) {
 
 app.get('/main', function (req, res) {
     res.sendFile(path.join(__dirname + '/data/MainPageInfo.json'))
+})
+
+app.get('/games/:game', function (req, res) {
+    const game = req.params.game
+    
+    fs.readFile( __dirname + '/data/GeneralQuestsInfo.json', 'utf8', (err, data) => {
+        const generalInfo = JSON.parse( data )
+        const relevant = generalInfo.filter(obj => {
+            return obj.alias === game
+        })
+        res.end(JSON.stringify(relevant))
+    })  
 })
 
 app.listen(3000, () => {
